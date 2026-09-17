a2b428408ce471f92032063de0049e10aee2bbf2
yosiefgobeze <yosiefgobeze@gmail.com>
2026-09-17 12:39:21 -0700

    Initial commit on online-grocery-order-management

---
+++ src/main/java/com/yosiefgobeze/onlinegrocery/OnlineGroceryOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.yosiefgobeze.onlinegrocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGroceryOrderManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineGroceryOrderManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yosiefgobeze/onlinegrocery/OnlineGroceryOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosiefgobeze.onlinegrocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGroceryOrderManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
